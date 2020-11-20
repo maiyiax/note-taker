f8399340964860183e1c7267f590fcef571fbf51
Mai Yia Xiong <maiyiax@gmail.com>
2020-11-20 12:09:03 -0600

    added delete route

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -10,8 +10,11 @@ const uniqid = require('uniqid');
 
 // setup api route
 router.get('/notes', (req, res) => {
-    // GET /api/notes should read the db.json file and return all saved notes as JSON            
-        res.json(db);
+    // GET /api/notes should read the db.json file and return all saved notes as JSON 
+    fs.readFile(path.join(__dirname, "../db/db,json"), (err, data) => {
+        if (err) throw err;
+    });           
+    res.json(db);
 });
 
 
@@ -34,13 +37,21 @@ router.post('/notes', (req, res) => {
 
 });
 
-// // Bonus: add DELETE route
-// // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete
+// Bonus: add DELETE route
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete
 router.delete('/notes/:id', (req, res) => {
-// // In order to delete a note, you'll need to read all notes from the db.json file
-// // remove the note with the given id property
-// // and then rewrite the notes to the db.json file
+    let id = req.params.id;
 
+    // In order to delete a note, you'll need to read all notes from the db.json file
+    fs.readFileSync(path.join(__dirname, "../db/db,json"));
+
+    // remove the note with the given id property
+    db.splice(id, 1);
+
+    // and then rewrite the notes to the db.json file
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify([{ db }], null, 2));
+
+    res.send(req.body);
 })
 
 module.exports = router;
